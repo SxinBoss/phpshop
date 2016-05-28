58b7952926304244add6e83b10fc13842956ae55
SxinBoss <boss@mrsong.xin>
2016-05-28 23:30:00 +0800

    Add log

--- conf.php
+++ conf.php
@@ -1,2 +1,8 @@
 <?php
     define("DB_Prefix", "shop");
+
+    define("DBIP",getenv("SHOPDBIP"));
+    define("DBPORT",getenv("SHOPDBPORT"));
+    define("DBNAME",getenv("SHOPDBNAME"));
+    define("DBUSER",getenv("SHOPDBUSER"));
+    define("DBPASS",getenv("SHOPDBPASS"));
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
     include "conf.php";
     echo DB_Prefix;
+
+
+
