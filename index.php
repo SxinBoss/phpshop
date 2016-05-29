218e7c45d69f3e2e3ff865928460ce7a0431d5f2
sxin <boss@mrsong.xin>
2016-05-29 11:55:04 +0800

    NotORM Conn OK

--- index.php
+++ index.php
@@ -2,10 +2,8 @@
     include "conf.php";
     include "NotORM.php";
     $dbbstr='mysql:host='.DBIP.';port='.DBPORT.';dbname='.DBNAME;
-    echo $dbbstr;
-    exit;
-    $pdo = new PDO('mysql:host='.DBIP.';port='.DBPORT.';dbname='.DBNAME,DBUSER,DBPASS);
-    $db = new NotORM($pdo);
-    echo $db;
-
-
+    $pdo = new PDO('mysql:host='.DBIP.';port='.DBPORT.'; dbname='.DBNAME,DBUSER,DBPASS,array(
+		PDO::ATTR_PERSISTENT=>true  
+	));  
+	
+	$db = new NotORM($pdo);
